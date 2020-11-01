271c1d6e6659cc16338b52bcd9b518d647add4d4
TheKenetics <TheKenetics@users.noreply.github.com>
2020-11-01 14:42:33 -0600

    Add more notes to zipit_gui

--- zipit_gui.py
+++ zipit_gui.py
@@ -9,10 +9,48 @@ Plan:
 	Additional features
 		Have checkmarks next to items to select which folders should be zipped
 		Have another text box that takes a string, and will select/deselect items based on startswith, endswith, and contains
+
+frame = tkinter.Frame(root, width=100, height=100)
+frame.pack()
+frame.pack(side=tkinter.BOTTOM)
+
+
+
+label = tkinter.Label(frame, text="asd", bg="black", fg="white")
+label.pack(side=tkinter.LEFT, fill=tkinter.X/Y)
+
+button = tkinter.Button(frame, text="asd", fg="red", command=funcname)
+button.bind("<Button-1>", funcname) # left mouse button click
+<Button-2> Middle click
+<Button-3> Right click
+button.pack()
+button.pack(side=tkinter.LEFT)
+
+entry = tkinter.entry(frame)
+entry.pack(side=tkinter.LEFT)
+entry.grid(row=0, column=0, sticky=tkinter.N/E/S/W, rowspan=2, columnspawn=2)
+sticky aligns things within cells, so sticky=E right aligns
+
+checkbox = tkinter.Checkbutton(frame, text="asd")
+checkbox.pack(side=LEFT)
+
+menu = tkinter.Menu(root)
+root.config(menu=menu)
+
+submenu = tkinter.Menu(menu)
+menu.add_cascade(label="File", menu=submenu)
+submenu.add_command(label="Do Something..." command=funcname)
+
+
+frame.quit function to quit
 """
 
 def main():
-	pass
+	root = tkinter.Tk()
+
+	
+
+	root.mainloop()
 
 if __name__ == "__main__":
 	main()
